53065022aa46cbdbdc99a67216b3d301f9a3dbdf
Dina Hafez <dhafez@natera.com>
2025-05-20 16:55:28 -0700

    Better interface

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, session
 from flask_cors import CORS
 from drug_extractor_agent import drug_names_extractor_agent, target_names_extractor_agent, prediction_agent, repurpose_agent
 
+
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'  # Required for session
+
 CORS(app)  # Enable CORS for all routes
 
 @app.route('/', methods=["GET", "POST"])
@@ -10,8 +13,118 @@ CORS(app)  # Enable CORS for all routes
 def index():
     return render_template('index.html')
 
+@app.route('/getMedicalInfo', methods=["GET", "POST"])
+def getMedicalInfo():
+    drugRepurpose = []
+    medical_info_drug=""
+    medical_info_protein=""
+
+    show_section_repurpose = session.get("show_section_repurpose")
+    show_section_protein = session.get("show_section_protein")
+    show_section_drug = session.get("show_section_drug")
+
+    if request.method == "POST":
+
+        try:
+            drugTarget = session.get('drugTarget') 
+            print ("Drug names is")
+            print (drugTarget)
+            if 'drugbtn' in request.form:
+                show_section_drug = True
+                medical_info_drug = "This is the info regarding drug"
+                session['medical_info_drug'] =medical_info_drug
+            elif 'proteinbtn' in request.form:
+                show_section_protein = True
+                medical_info_protein = "This is the info regarding protein"
+                session['medical_info_protein'] =medical_info_protein
+            elif 'repurposebtn' in request.form:
+                show_section_repurpose = True
+                target = drugTarget[0]['Target']
+                #modify this part
+                
+                drugRepurpose.append({'Rank': 1,'Drug':"dn",'Target':target,'Score':2.5})
+                drugRepurpose.append({'Rank': 2,'Drug':"dn2",'Target':target,'Score':3.5})
+                '''result = repurpose_agent(target)
+                drugRepurpose = []
+                for row in result:
+                    rank = row[0]  # Index 0 → "Name"
+                    dn = row[1]
+                    tn = row[2]
+                    db_score = row[3]
+                    drugRepurpose.append({'Rank': rank,'Drug':dn,'Target':tn,'Score':db_score})
+                '''
+                session['drugRepurpose'] = drugRepurpose
+            
+            medical_info_drug = session.get("medical_info_drug")
+            medical_info_protein = session.get("medical_info_protein")
+            drugRepurpose = session.get("drugRepurpose")
+
+            session['show_section_repurpose'] = show_section_repurpose
+            session['show_section_protein'] = show_section_protein
+            session['show_section_drug'] = show_section_drug
+
+            return render_template("index.html", medical_info_drug=medical_info_drug,medical_info_protein=medical_info_protein,drugTarget=drugTarget,show_section_drug=show_section_drug,show_section_protein=show_section_protein,show_section_repurpose=show_section_repurpose,show_section=True, drugRepurpose=drugRepurpose)
+
+        except Exception as e:
+            print(f"Error: {e}")
+            return render_template('wrong_index.html', error=str(e)), 500
+    return render_template("index.html")
+
+
 @app.route('/extractDrugNames', methods=["GET", "POST"])
 def extractDrugNames():
+    if request.method == "POST":
+        try:
+            show_section = True
+            proposal = request.form.get('proposal')
+            print (proposal)
+            print ("Testing printing proposal drugs", flush = True)
+            #drug_names = "Aspirin"
+            drug_names = drug_names_extractor_agent(proposal)
+            print("\n The proposal is: ")
+            print (proposal)
+            print("\n The drugs you are using in this proposal are: ")
+            print (drug_names)
+            # Call the agent to extract target names
+            #target_names = "COX1,COX2"
+            target_names = target_names_extractor_agent(proposal)
+            #print("\n The target proteins that the above drugs are binding to in this proposal are here in app.py: ")
+            print (target_names)
+            score = prediction_agent(drug_names, target_names)
+            #score=[2.5,3.5]
+
+            drugTarget = []
+            if isinstance(score, (int, float)):
+                print ("it is a float")
+                #scorelist.append = str(round(score,2))
+                drugTarget.append({'Drug':drug_names,'Target':target_names,'Score':round(score,2)})
+
+            else:
+                for i in range(len(score)):
+                    print ("Item number ")
+                    print (i)
+                    print (target_names.split(",")[i])
+                    print (score[i])
+                    drugTarget.append({'Drug':drug_names,'Target':target_names.split(",")[i],'Score':round(score[i],2)})
+
+            session['drugTarget'] =drugTarget
+            session['show_section_repurpose'] = False
+            session['show_section_protein'] = False
+            session['show_section_drug'] = False
+            session['medical_info_drug'] = ""
+            session['medical_info_protein'] = ""
+            session['drugRepurpose'] = ""
+            
+
+            return render_template("index.html", drug_names=drug_names, target_names=target_names, score=score,drugTarget=drugTarget,show_section=show_section)
+        except Exception as e:
+            print(f"Error: {e}")
+            return render_template('wrong_index.html', error=str(e)), 500
+
+    return render_template("index.html")
+
+@app.route('/doRepurpose', methods=["GET", "POST"])
+def doRepurpose():
     if request.method == "POST":
         try:
             show_section = True
@@ -65,7 +178,6 @@ def extractDrugNames():
             return render_template('wrong_index.html', error=str(e)), 500
 
     return render_template("index.html")
-
 
 if __name__ == '__main__':
      app.run(host='0.0.0.0', port=9999)
